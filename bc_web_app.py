bb560e9b8a7004e1859fdbfc0e317930ebf0e33a
JACINTA CHEGE <64266410+Jess-chege@users.noreply.github.com>
2025-01-23 10:44:16 +0300

    update bc_web_app.py

--- bc_web_app.py
+++ bc_web_app.py
@@ -41,6 +41,49 @@ def main():
     
     #getting the input data from user
     mean_radius_input = input("Enter the patients' mean_radius: ")
+    mean_texture_input = input("Enter the patients' mean_texture")
+    mean_perimeter_input= input("Enter the patients' mean_perimeter")
+    mean_area_input = input("Enter the patients' mean_area")
+    mean_smoothness_input= input("Enter the patients' mean_smoothness")
+    
+    
+    
+#numeric conversions
+
+    mean_radius = pd.to_numeric(mean_radius_input, errors='coerce')
+    mean_texture = pd.to_numeric(mean_texture_input, errors='coerce')
+    mean_perimeter= pd.to_numeric(mean_perimeter_input, errors='coerce')
+    mean_area = pd.to_numeric(mean_area_input, errors='coerce')
+    mean_smoothness= pd.to_numeric(mean_smoothness_input, errors='coerce')
+   
+
+    diagnosis = ''
+    
+    #creating a prediction button
+    if st.button("PREDICT"):
+        diagnosis = breast_cancer_disease([mean_radius,mean_texture,mean_perimeter,mean_area,mean_smoothness])
+    st.success(diagnosis)
+    
+if __name__ =='__main__':
+    main()
+
+    
+    
+    
+    
+    
+    if prediction [0] == 0:
+        return"The person does not have breast cancer"
+    else:
+        return"The person has breast cancer"
+        
+#streamlit library  to create a user interface
+def main():
+    
+    st.title("Breast Cancer Prediction Machine Learning Model")
+    
+    #getting the input data from user
+    mean_radius_input = input("Enter the patients' mean_radius: ")
     mean_texture = input("Enter the patients' mean_texture")
     mean_perimeter= input("Enter the patients' mean_perimeter")
     mean_area = input("Enter the patients' mean_area")
